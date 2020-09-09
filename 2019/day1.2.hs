0400af1975dad1af0a7d4b6ff44ee5aeb5737f57
Masa Maeda <masamaedae@gmail.com>
2020-09-08 21:46:47 -0700

    Allows the user to see the recursion steps

--- 2019/day1.2.hs
+++ 2019/day1.2.hs
@@ -6,10 +6,10 @@ doCalc :: Int -> Int
 doCalc x =
   floor (fromIntegral (x) / 3) - 2
 
-recurseCalc :: Int -> Int -> Int
+recurseCalc :: [Int] -> Int -> [Int]
 recurseCalc total x
-  | value <= 0 = total
-  | otherwise = recurseCalc (total + value) value
+  | value <= 0 = reverse total
+  | otherwise = recurseCalc (value : total) value
   where
     value = doCalc x
 
@@ -23,9 +23,11 @@ mainHelper total =
         if null line
           then putStrLn $ "Total: " ++ (show total)
           else do
-            let fuel = recurseCalc 0 (read line)
-            putStrLn (show fuel)
-            mainHelper $ total + fuel
+            let fuel = recurseCalc [] (read line)
+            let totalFuel = sum fuel
+            putStrLn $ "Steps: " ++ show fuel
+            putStrLn $ "Fuel: " ++ show totalFuel
+            mainHelper $ total + totalFuel
 
 main = do
   mainHelper 0
