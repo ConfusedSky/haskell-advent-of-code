33500edbadcc1930434e4173da8e6a4a3855ab01
Masa Maeda <masamaedae@gmail.com>
2020-09-13 15:01:02 -0700

    Updates day2 to use a sequence

--- 2019/day2.hs
+++ 2019/day2.hs
@@ -1,43 +1,46 @@
-import Data.List
-import System.Environment
-import System.IO
+import Data.Foldable (Foldable (null))
+import Data.Sequence as Seq (Seq, fromList, index, update, (!?))
+import System.IO (isEOF)
 
-getOps array index =
+getOps :: Seq Int -> Int -> (Int, Int, Int)
+getOps array i =
   (op1, op2, op3)
   where
-    op1 = array !! (array !! (index + 1))
-    op2 = array !! (array !! (index + 2))
-    op3 = array !! (index + 3)
+    op1 = array `index` (array `index` (i + 1))
+    op2 = array `index` (array `index` (i + 2))
+    op3 = array `index` (i + 3)
 
-replaceNth :: Int -> a -> [a] -> [a]
-replaceNth _ _ [] = []
-replaceNth n newVal (x : xs)
-  | n == 0 = newVal : xs
-  | otherwise = x : replaceNth (n -1) newVal xs
-
+performOp :: (Int -> Int -> Int) -> Seq Int -> Int -> Seq Int
 performOp op array index =
   newArray
   where
     (op1, op2, op3) = getOps array index
     newValue = op op1 op2
-    newArray = replaceNth op3 newValue array
+    newArray = update op3 newValue array
 
+handleCalcs :: Seq Int -> Int -> IO ()
 handleCalcs array index =
   do
     putStrLn $ "Index: " ++ show index
     putStrLn $ show array
 
-    case array !! index of
-      1 -> do
+    case array !? index of
+      Just 1 -> do
         putStrLn "Sum"
         handleCalcs (performOp (+) array index) $ index + 4
-      2 -> do
+      Just 2 -> do
         putStrLn "Multiply"
         handleCalcs (performOp (*) array index) $ index + 4
-      99 ->
+      Just 99 ->
         putStrLn "Exit"
       _ ->
         putStrLn "Invalid opcode"
+
+startCalc :: [Int] -> IO ()
+startCalc array =
+  handleCalcs s 0
+  where
+    s = fromList array
 
 main = do
   iseof <- isEOF
@@ -45,8 +48,8 @@ main = do
     then putStrLn $ "No input!"
     else do
       line <- getLine
-      if null line
+      if Data.Foldable.null line
         then putStrLn $ "No input"
         else do
           let array = read line :: [Int]
-          handleCalcs array 0
+          startCalc array
