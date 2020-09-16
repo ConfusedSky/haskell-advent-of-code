fd9a207a2b6ab98f03de378eac1ee0dd52ef49d0
Masa Maeda <masamaedae@gmail.com>
2020-09-15 20:56:01 -0700

    Has the line collision function return a maybe point instead of a bool

--- 2019/day3.hs
+++ 2019/day3.hs
@@ -4,27 +4,32 @@ data Point = Point
   { x :: Int,
     y :: Int
   }
+  deriving (Show)
 
-data Direction = U | R deriving (Eq)
+data Direction = U | R deriving (Eq, Show)
 
 data LineSegment = LineSegment
   { origin :: Point,
     direction :: Direction,
     len :: Int
   }
+  deriving (Show)
 
 createSegment x y d l =
   LineSegment {origin = Point {x, y}, direction = d, len = l}
 
-lineCollision :: LineSegment -> LineSegment -> Bool
+lineCollision :: LineSegment -> LineSegment -> Maybe Point
 lineCollision first second =
-  not parallel
-    && case direction first of
+  if parallel
+    then Nothing
+    else case direction first of
       U ->
-        ox first > ox second
+        if ox first > ox second
           && ox first < ox second + len second
           && oy second > oy first
           && oy second < oy first + len first
+          then Just $ Point (ox first) (oy second)
+          else Nothing
       -- Handle only one of the two cases
       -- And flip the other
       R -> lineCollision second first
